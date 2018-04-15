77109674985f1f3eb09c1b58a95af383ea73d769
Bruno Fagundez <bfagundez@gmail.com>
2018-04-15 14:37:53 -0700

    Validated Teams model behavior

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @team = teams(:one)
+  end 
+
+  test 'valid team' do
+    assert @team.valid?
+  end
+
+  test '#organization' do
+    assert_instance_of Organization, @team.organization
+  end
 end
