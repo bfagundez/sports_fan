57c67012aa1f94e74640eea9097a382944b6f032
Bruno Fagundez <bfagundez@gmail.com>
2018-04-16 00:00:40 -0700

    Added skeleton for organizations list

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -2,21 +2,26 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Footer from "components/Footer"
 import Header from "components/Header"
-import Home from "containers/Home"
+import Organizations from "containers/Organizations/index.js"
+import Home from "containers/Home/index.js"
 
 const App = () => (
   <div className="App">
     <Header />
-    <Router>
-      <div>
+      <Router>
+        <div className="app-content">
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
+          <li>
+           <Link to="/organizations">Organization</Link>
+          </li>
         </ul>
         <Route exact path="/" component={Home} />
-      </div>
-    </Router>
+        <Route exact path="/organizations" component={Organizations} />
+        </div>
+      </Router>
     <Footer />
   </div>
 );
---
+++ client/src/containers/Organizations/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Organizations extends Component {
+  render() {
+    return (
+      <p> This is organization list </p> 
+    );
+  }
+}
+
+
+export default Organizations;
