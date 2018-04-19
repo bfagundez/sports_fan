7993c28e66fb640346518e0790b963454bc8815a
Bruno Fagundez <bfagundez@gmail.com>
2018-04-18 23:02:27 -0700

    Added organization seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Organization.create([{ name: "Faraday Academy"},
+  { name: "Littlewood College"},
+  { name: "Pine Hills School of Fine Arts"},
+  { name: "Lakewood School"},
+  { name: "Meadows Conservatory"},
+  { name: "Spring Gardens Technical School"},
+  { name: "Eastview College"},
+  { name: "G reen Valley Middle School"},
+  { name: "Cypress Grammar School"}])
