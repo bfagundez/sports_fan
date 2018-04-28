886d58799c18062f180c1417281dad1abf1dbe19
Bruno Fagundez <bfagundez@gmail.com>
2018-04-28 10:48:53 -0700

    Added flaticon font with sport icons

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './fonts/flaticon.css';
 import App from 'containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.css'
